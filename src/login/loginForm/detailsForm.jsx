c3a2b16404dc6ef7978b9b73d8c28b8697521cd2
Abhinav Rana <48202760+ranaabhinav726@users.noreply.github.com>
2023-02-13 12:53:16 +0530

    changes

--- src/login/loginForm/detailsForm.jsx
+++ src/login/loginForm/detailsForm.jsx
@@ -301,7 +301,7 @@ if(type==='Entity'){
             },
           ]}
         >
-          <Input className="background-color" placeholder="Pincode" type='tel'  max='6'/>
+          <Input className="background-color" placeholder="Pincode" type='tel'  maxLength='6'/>
         </Form.Item>
         <Form.Item
           label="City"
